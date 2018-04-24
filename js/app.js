440076c996fbaa0f1f7a705718e1a99c38349065
Charles Hedji <charleshedji@gmail.com>
2018-04-24 01:14:05 -0400

    added jquery and finished infowindow

--- js/app.js
+++ js/app.js
@@ -1,7 +1,14 @@
+'use strict'
+
 //Model
 var map;
 
 var markers = [];
+
+var clientID = 'JHJQ23XA1HX0ODEAUZRDXC3YLHSWMH45FTBA45H01031RMQ3';
+//'OxfUCyWyBmScEzS5RWExCg';
+var clientSecret = 'WCWRQTUIOED5Y23S3TCMDUVHSZIUWQMNPALN5FE4CX34G4U2';
+//'r8tb-Bo8uQSOf9qLMYcAHjlPEK07XkbfWfu03PjSSqo9pKp0HRTBlFFQPJ3wXv1f8KkQlML65oXZmceNlSuuoriZyhZTC7XK4vXI6h1THCAzc6sxJGr3-qXo19DUWnYx';
 
 function initMap(){
 	var styles = [
@@ -24,11 +31,20 @@ function initMap(){
 			stylers: [{color: '#d59563'}]
 		}
 	]
-	map= new google.maps.Map(document.getElementById('map'), {
+	map = new google.maps.Map(document.getElementById('map'), {
 		center: {lat:40.767499, lng:-73.833079},
 		zoom: 13,
 		styles: styles,
-		mapTypeControl: false
+		mapTypeControl: false,
+		zoomControl: true,
+		zoomControlOptions: {
+			position: google.maps.ControlPosition.LEFT_CENTER
+		},
+		streetViewControl: true,
+		//this can be dragged across the map to get street view
+		streetViewControlOptions: {
+			position: google.maps.ControlPosition.LEFT_TOP
+		}
 	});
 	var bounds = new google.maps.LatLngBounds();
 
@@ -58,7 +74,23 @@ function initMap(){
 		marker.addListener('click', function(){
 			populateInfoWindow(this, largeInfowindow);
 		});
+
+		marker.addListener('mouseover', function(){
+			this.setIcon(highlightedIcon);
+		});
+		marker.addListener('mouseout', function(){
+			this.setIcon(defaultIcon);
+		});
+
+		locations[i].marker = marker;
+
+		marker.setMap(map);
+		bounds.extend(marker.position);
+
+
 	}
+	map.fitBounds(bounds);
+
 	ko.applyBindings(new ViewModel());
 }
 
@@ -73,31 +105,34 @@ var locations = [
 	{title: "PappaRich", location: {lat: 40.7595570,lng: -73.8323920}}
 ];
 
+function populateInfoWindow(marker, infowindow){
+	if(infowindow.marker != marker){
+		infowindow.marker = marker;
+		
+		//FourSquare Api
+		//the ll in the link refers to the latitude and longitude
+		var fsquare = 'https://api.foursquare.com/v2/venues/search?ll=' + marker.position.lat() + ',' + marker.position.lng() +'&client_id=' + clientID + '&client_secret=' + clientSecret + '&v=20180422';
+		
+		$.getJSON(fsquare, function(data){
+			var fsqResponse = data.response.venues[0];
+			var fsquare = 'https://foursquare.com/v/' + fsqResponse.id;
+		
+		//infowindow contains content derived from FourSquare API
+		infowindow.setContent('<div>' +marker.title+ '<div><strong>FourSquare Link: </strong>' + '<a href="' + fsquare + '">' + fsqResponse.name + '</a></div>' +
+			'<div><strong>Address: </strong>' + fsqResponse.location.formattedAddress + '</div></div>');
+		infowindow.open(map, marker);
+	})
+		infowindow.addListener('closeclick', function(){
+			infowindow.marker = null;
+		});
+		
+		
+	}
+}
 
 
 
-var Places = function(data){
-	this.title = ko.observable(data.title);
-	this.lat = ko.observable(data.location.lat);
-	this.lng = ko.observable(data.location.lng);
-	this.marker = ko.observable(data.marker);
-};
-//ViewModel: what the user interacts with
-var ViewModel = function(){
-	var self = this;
-
-	
-
-	
-
-	
-
-	
-
-	this.showFood = document.getElementById('show-food');
-	this.hideFood = document.getElementById('hide-food');
-
-	function makeMarkerIcon(markerColor){
+function makeMarkerIcon(markerColor){
 		var markerImage = new google.maps.MarkerImage(
 			'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
 		'|40|_|%E2%80%A2',
@@ -108,25 +143,43 @@ var ViewModel = function(){
 		);
 		return markerImage;
 	}
+var Places = function(data){
+	this.title = ko.observable(data.title);
+	this.lat = ko.observable(data.location.lat);
+	this.lng = ko.observable(data.location.lng);
+	this.marker = ko.observable(data.marker);
+};
 
-	self.showFood = function(spotsItem){
-		var bounds = new google.maps.LatLngBounds();
-		for(var i = 0; i < markers.length; i++){
-			markers[i].setMap(map);
-			bounds.extend(markers[i].position);
+
+//ViewModel: what the user interacts with
+var ViewModel = function(){
+	var self = this;
+
+	this.eateries = ko.observableArray([]);
+
+	locations.forEach(function(eateryItem){
+		self.eateries.push(new Places(eateryItem));
+	});
+	//this trigger with select the clicked location on the map
+	this.clickLocation = function(clickedData){
+		var clickedMarker = clickedData.marker();
+		google.maps.event.trigger(clickedMarker, 'click');
+	};
+
+	this.textVals = ko.observable("");
+	//this filters the list according to the user input
+	this.filterList = ko.computed(function(){
+		if(self.textVals !== ""){
+			var newString = self.textVals();
+			self.eateries.removeAll();
+			locations.forEach(function(eateryItem){
+				eateryItem.marker.setMap(null);
+				if(eateryItem.title.includes(newString)){
+					self.eateries.push(new Places(eateryItem));
+					eateryItem.marker.setMap(map);
+				}
+			})
 		}
-		map.fitBounds(bounds);
-	}
+	}, this);
+};
 
-}
-
-
-//*var Spots = function(data){
-//	this.name = ko.observable(data.title);
-//	this.lat = ko.observable(data.lat);
-//	this.lng = ko.observable(data.lng);
-//	this.marker = ko.observable('');
-//	this.showFood = ko.observable('');
-//	this.hideFood = ko.observable(''); 
-
-//}
